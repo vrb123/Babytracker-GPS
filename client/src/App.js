330c45c97587f8915e35b43240a6b2574f4ceead
vitalii <vitalii.vorobij@gmail.com>
2019-10-03 12:35:44 +0300

    added login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useContext} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom";
 
-function App() {
+import Header from './components/Header';
+import Login from './components/Main/Login';
+import Register from './components/Main/Register';
+
+import {LoginProvider} from './contexts/LoginContext';
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <LoginProvider>
+        <Router>
+          <Header/>
+          <Switch>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            <Route exact path="/register">
+                <Register />
+            </Route>
+          </Switch>
+        </Router>
+    </LoginProvider>
+    
+  )
+};
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+const routes = require('./routes');
+
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+
+// // parse application/json
+app.use(bodyParser.json());
+
+// app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//Connect to DB
+mongoose.connect(config.dbConnection, {useUnifiedTopology: true, useNewUrlParser: true } ,() => {
+    console.log('Connected to database');
+});
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
@@ -16,6 +35,10 @@ app.get('/', (req,res) => {
     res.send('Hello world');
 });
 
-app.listen(PORT,() => {
-    console.log(`Listening to ${PORT} port`);
+app.use('/customer',routes.customer);
+
+
+
+app.listen(config.PORT,() => {
+    console.log(`Listening to ${config.PORT} port`);
 });

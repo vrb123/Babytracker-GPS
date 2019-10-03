dd048d86fcd46e01006d06d57b7a003f83f6acc2
vitalii <vitalii.vorobij@gmail.com>
2019-10-03 07:28:31 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => { 
+         res.sendFile(path.join(__dirname+'/client/public/index.html'));
+    });
+}
+
+app.get('/', (req,res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT,() => {
+    console.log(`Listening to ${PORT} port`);
+});
